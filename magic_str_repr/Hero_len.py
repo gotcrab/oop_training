989cafac2cdc95ad77939ca32701e3793fef8304
gotcrab <kuznetsov_alx@proton.me>
2023-02-26 23:57:48 +0300

    Create Hero for description heroes!!!

---
+++ magic_str_repr/Hero_len.py
@@ -0,0 +1,38 @@
+class Hero:
+    'Description the superhero!'
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __str__(self):
+        res = ''
+        for i,j in sorted(self.__dict__.items()):
+            res += f'{str(i)}: {str(j)}\n'
+
+        return res.rstrip('\n')
+
+hero = Hero()
+assert len(hero) == 0
+hero.health = 50
+hero.damage = 10
+assert len(hero) == 2
+assert str(hero) == '''damage: 10
+health: 50'''
+hero.weapon = ['sword', ' bow']
+hero.skill = 'Некромант'
+assert str(hero) == '''damage: 10
+health: 50
+skill: Некромант
+weapon: ['sword', ' bow']'''
+print(hero)
+
+villain = Hero()
+assert str(villain) == ''
+assert len(villain) == 0
+villain.level = 15
+villain.skill = 'Боец'
+villain.armor = 25
+assert len(villain) == 3
+assert str(villain) == '''armor: 25
+level: 15
+skill: Боец'''
+print(villain)
