99f7c1f2fccd4bd98cf883f5a2f5e43f1ec61598
gotcrab <kuznetsov_alx@proton.me>
2023-02-27 23:48:01 +0300

    Add the ability to plus and to multiply vectors

--- magic_str_repr/Vector.py
+++ magic_str_repr/Vector.py
@@ -14,6 +14,41 @@ class Vector:
             return f'Вектор{tuple(sorted(self.values))}'
         return f'Пустой вектор'
 
+    def __add__(self, other):
+        if type(other) == int:
+            new_val = []
+            for i in self.values:
+                new_val.append(i + other)
+            return Vector(*new_val)
+        if isinstance(other, Vector):
+            if sorted(self.values) == sorted(other.values):
+                new_val = []
+                for i in sorted(self.values):
+                    new_val.append(i * 2)
+                return Vector(*new_val)
+            else:
+                return 'Сложение векторов разной длины недопустимо'
+        return f'Вектор нельзя сложить с {other}'
+
+    def __mul__(self, other):
+        if type(other) == int:
+            new_val = []
+            for i in self.values:
+                new_val.append(i * other)
+            return Vector(*new_val)
+        if isinstance(other, Vector):
+            if sorted(self.values) == sorted(other.values):
+                new_val = []
+                for i in sorted(self.values):
+                    new_val.append(i ** 2)
+                return Vector(*new_val)
+            else:
+                return 'Умножение векторов разной длины недопустимо'
+        return f'Вектор нельзя умножать с {other}'
+
+
+
+
 #tests:
 
 v1 = Vector(2, 3, 1)
@@ -37,3 +72,12 @@ print(v1)
 print(v2)
 print(v3)
 print(v4)
+
+v5 = v1 + 2
+print(v5)
+
+v6 = v1 + 'Hello'
+print('v6 =', v6)
+
+v7 = v1 * True
+print('v7 =', v7)
