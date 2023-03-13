903b2ce1c5c775be9d7d741e882c4f25248f08b3
gotcrab <kuznetsov_alx@proton.me>
2023-03-13 17:23:56 +0300

    Create Person_2.py

---
+++ inheritance/Person_2.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_person_info(self):
+        print(f'Person: {self.name}, {self.age}')
+
+class Company:
+    def __init__(self, company_name, location):
+        self.company_name = company_name
+        self.location = location
+
+    def display_company_info(self):
+        print(f'Company: {self.company_name}, {self.location}')
+
+class Employee(Person, Company):
+    def __init__(self, name, age, company_name, location):
+        super().__init__(name, age)
+        Company.__init__(self, company_name, location)
+
+if __name__ == '__main__':
+    emp = Employee('Jessica', 28, 'Google', 'Atlanta')
+    emp.display_person_info()
+    emp.display_company_info()
