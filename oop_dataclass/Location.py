87d4bc2283f568498519c4a8bd4bace13d005a56
gotcrab <kuznetsov_alx@proton.me>
2023-03-17 01:24:43 +0300

    Create Location.py

---
+++ oop_dataclass/Location.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass()
+class Location:
+    name: str
+    longitude: float = 0
+    latitude: float = 11.5
+
+
+stonehenge = Location('Stonehenge', 51, 1.5)
+
+print(stonehenge)
